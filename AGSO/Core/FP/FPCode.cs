921aaf99ca23d129fd17f21aefe02db57cc2153a
Wu Zhenwei <wuzhenwei35@gmail.com>
2016-10-20 14:23:42 -0400

    Remove some unused code.

--- AGSO/Core/FP/FPCode.cs
+++ AGSO/Core/FP/FPCode.cs
@@ -11,20 +11,6 @@ namespace AGSO.Core.FP
 {
     class FPCode
     {
-        private static byte[][] _Template = new byte[][]
-        {
-            new byte[] { 0x0F, 0xAE, 0x1D, 0x00, 0x00, 0x00, 0x00 }, //stmxcsr [?]
-            new byte[] { 0xB8, 0x00, 0x00, 0x00, 0x00 }, //mov eax, ?
-            new byte[] { 0x09, 0x05, 0x00, 0x00, 0x00, 0x00 }, //or [?], eax
-            new byte[] { 0x0F, 0xAE, 0x15, 0x00, 0x00, 0x00, 0x00 }, //ldmxcsr [?]
-            new byte[] { 0x9B, 0xD9, 0x3D, 0x00, 0x00, 0x00, 0x00 }, //fstcw [?]
-            new byte[] { 0xB8, 0x00, 0x00, 0x00, 0x00 }, //mov eax, ?
-            new byte[] { 0x09, 0x05, 0x00, 0x00, 0x00, 0x00 }, //or [?], eax
-            new byte[] { 0xD9, 0x2D, 0x00, 0x00, 0x00, 0x00 }, //fldcw [?]
-            new byte[] { 0x31, 0xC0 }, //xor eax, eax
-            new byte[] { 0xC3 }, //ret
-        };
-
         [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
         private delegate int GeneratedDelegate();
 
@@ -32,7 +18,6 @@ namespace AGSO.Core.FP
         {
             var raw = AssemblyCodeStorage.WriteCode(Generate());
             GeneratedDelegate f = (GeneratedDelegate)Marshal.GetDelegateForFunctionPointer(raw, typeof(GeneratedDelegate));
-            System.Windows.Forms.MessageBox.Show("fp");
             f();
         }
 
