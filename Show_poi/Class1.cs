f2382925f2e6f0b10b3336dff7db87dda412e681
SakuraKusako <m15813290160@163.com>
2016-10-23 00:39:43 +0800

    Show_poi

---
+++ Show_poi/Class1.cs
@@ -0,0 +1,50 @@
+﻿using PluginUtils;
+using PluginUtils.Injection.Squirrel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Show_poi
+{
+    public class Class1 : IAMLPlugin
+    {
+        private static IntPtr _IntBuffer = Marshal.AllocHGlobal(4);
+        private static int type_poi; //信息类型
+        private static int value_poi; //信息的值
+        private static MessagePoi text_poi; //窗口对象
+
+        public void Init()
+        {
+            SquirrelHelper.RegisterGlobalFunction("SendMessagePoi", GetMessagePoi);
+            PluginUtils.WindowsHelper.Run(delegate ()
+            {
+                text_poi = new MessagePoi();
+                //text_poi.Show();
+                new System.Windows.Forms.Form().Show();
+            });
+        }
+
+        public void Load()
+        {
+        }
+
+        public static int GetMessagePoi(IntPtr p)
+        {
+            SquirrelFunctions.getinteger(p, 2, _IntBuffer);
+            type_poi = Marshal.ReadInt32(_IntBuffer);
+            SquirrelFunctions.getinteger(p, 3, _IntBuffer);
+            value_poi = Marshal.ReadInt32(_IntBuffer);
+            ShowMessagePoi();
+            return 0;
+        }
+
+        public static void ShowMessagePoi()
+        {
+            text_poi.textBox1.AppendText("poi " + type_poi + " " + value_poi + "\n");
+        }
+    }
+}
---
+++ Show_poi/MessagePoi.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace Show_poi
+{
+    partial class MessagePoi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Info;
+            this.textBox1.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.textBox1.Location = new System.Drawing.Point(3, 5);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(528, 321);
+            this.textBox1.TabIndex = 0;
+            // 
+            // MessagePoi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(534, 326);
+            this.Controls.Add(this.textBox1);
+            this.Name = "MessagePoi";
+            this.Text = "MessagePoi";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.TextBox textBox1;
+    }
+}
