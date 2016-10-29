8fd3c81bf4098c77109ef006988fae6a0a1b84a3
akemimadoka <jxhzq1996@126.com>
2016-10-29 17:13:32 +0900

    Add project debugger

---
+++ Debugger/DebuggerPlugin.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PluginUtils;
+using PluginUtils.Injection.Squirrel;
+
+namespace Debugger
+{
+    public interface IMessageHandler
+    {
+        void DebugInfomationArrived(int type, string srcname, int line, string funcname);
+        void CompilerExceptionArrived(string exceptiondesc, string srcname, int line, int column);
+        void RuntimeExceptionArrived(string exceptiondesc);
+    }
+
+    public class DebuggerPlugin : IAMLPlugin
+    {
+        private readonly List<IMessageHandler> _messageHandlers = new List<IMessageHandler>();
+        private DebuggerWindow _window;
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        private delegate void CompilerErrorHandler(IntPtr vm, [MarshalAs(UnmanagedType.LPStr)]string exceptiondesc, [MarshalAs(UnmanagedType.LPStr)]string srcname, int line, int column);
+
+        public DebuggerPlugin()
+        {
+        }
+
+        public void RegisterMessageHandler(IMessageHandler handler)
+        {
+            _messageHandlers.Add(handler);
+        }
+
+        public void Init()
+        {
+            SquirrelHelper.RegisterGlobalFunction("DebugHook", vm =>
+            {
+                int type;
+                string srcname;
+                int line;
+                string funcname;
+                SquirrelFunctions.getinteger(vm, 2, out type);
+                SquirrelFunctions.getstring(vm, 3, out srcname);
+                SquirrelFunctions.getinteger(vm, 4, out line);
+                SquirrelFunctions.getstring(vm, 5, out funcname);
+
+                foreach (var handler in _messageHandlers)
+                {
+                    handler.DebugInfomationArrived(type, srcname, line, funcname);
+                }
+
+                return 0;
+            });
+
+            SquirrelHelper.RegisterGlobalFunction("ErrorHandler", vm =>
+            {
+                SquirrelFunctions.tostring(vm, 2);
+                string exceptionDescription;
+                SquirrelFunctions.getstring(vm, -1, out exceptionDescription);
+                SquirrelFunctions.poptop(vm);
+
+                foreach (var handler in _messageHandlers)
+                {
+                    handler.RuntimeExceptionArrived(exceptionDescription);
+                }
+
+                return 0;
+            });
+
+            SquirrelHelper.Run(vm =>
+            {
+                SquirrelFunctions.enabledebuginfo(vm, 1);
+
+                SquirrelHelper.InjectCompileFile("boot.nut", "main").AddBefore(
+                    sqvm =>
+                    {
+                        SquirrelFunctions.pushroottable(vm);
+                        SquirrelFunctions.pushstring(vm, "DebugHook", -1);
+                        SquirrelFunctions.get(vm, -2);
+                        SquirrelFunctions.setdebughook(vm);
+                        SquirrelFunctions.pushstring(vm, "ErrorHandler", -1);
+                        SquirrelFunctions.get(vm, -2);
+                        SquirrelFunctions.seterrorhandler(vm);
+                        SquirrelFunctions.poptop(vm);
+                    });
+                
+                var compilerErrorHandler = Marshal.GetFunctionPointerForDelegate((CompilerErrorHandler)((sqvm, desc, src, line, column) =>
+                {
+                    foreach (var handler in _messageHandlers)
+                    {
+                        handler.CompilerExceptionArrived(desc, src, line, column);
+                    }
+                }));
+
+                SquirrelFunctions.setcompilererrorhandler(vm, compilerErrorHandler);
+            });
+
+            WindowsHelper.Run(() =>
+            {
+                _window = new DebuggerWindow(this);
+                _window.Show();
+            });
+        }
+
+        public void Load()
+        {
+        }
+
+        public void SuspendVm()
+        {
+            SquirrelHelper.Run(vm =>
+            {
+                SquirrelFunctions.suspendvm(vm);
+            });
+        }
+
+        public void WakeupVm(bool resumedret, bool retval, bool raiseerror)
+        {
+            SquirrelHelper.Run(vm =>
+            {
+                SquirrelFunctions.wakeupvm(vm, resumedret ? 1 : 0, retval ? 1 : 0, raiseerror ? 1 : 0);
+            });
+        }
+    }
+}
---
+++ Debugger/DebuggerWindow.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Debugger
+{
+    public partial class DebuggerWindow : Form
+    {
+        private readonly DebuggerPlugin _plugin;
+        private class MessageHandler : IMessageHandler
+        {
+            private readonly DebuggerWindow _window;
+
+            public MessageHandler(DebuggerWindow window)
+            {
+                _window = window;
+            }
+
+            public void DebugInfomationArrived(int type, string srcname, int line, string funcname)
+            {
+                var ctype = (char)type;
+                if (ctype != 'l')
+                {
+                    // Too slow, faster method requested= =
+                    //_window.textBox1.Text += $"type {(char)type}, source {srcname}, line {line}, funcname {funcname}.{Environment.NewLine}";
+                }
+            }
+
+            public void CompilerExceptionArrived(string exceptiondesc, string srcname, int line, int column)
+            {
+                _window.textBox1.Text += $"Unhandled compiler exception detected, description: {exceptiondesc}, source: {srcname}, line: {line}, column: {column}.{Environment.NewLine}";
+            }
+
+            public void RuntimeExceptionArrived(string exceptiondesc)
+            {
+                _window.textBox1.Text += $"Unhandled exception detected, description: {exceptiondesc}.{Environment.NewLine}";
+                _window._plugin.SuspendVm();
+            }
+        }
+
+        public DebuggerWindow(DebuggerPlugin plugin)
+        {
+            _plugin = plugin;
+            _plugin.RegisterMessageHandler(new MessageHandler(this));
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _plugin.WakeupVm(true, true, true);
+        }
+    }
+}
