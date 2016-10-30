403fea86daa3a2fcdf83743d63601225f22acdac
Wu Zhenwei <wuzhenwei35@gmail.com>
2016-10-29 22:08:37 -0400

    Add ws api.

---
+++ AGSO/Network/Connection.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AGSO.Network
+{
+    class Connection
+    {
+        public static Connection CreateServer(int port)
+        {
+            return null;
+        }
+
+        public static Connection CreateClient(int port)
+        {
+            return null;
+        }
+
+        private IntPtr _Socket;
+        private Thread _Thread;
+        private volatile bool _StopThreadFlag;
+
+        private Connection()
+        {
+        }
+
+        private void StartThread()
+        {
+            while (!_StopThreadFlag)
+            {
+            }
+        }
+
+        public void Release()
+        {
+            if (_Thread.ThreadState == ThreadState.Running)
+            {
+                _StopThreadFlag = true;
+                _Thread.Join();
+            }
+            //close
+        }
+    }
+}
---
+++ NetworkTest/Program.cs
@@ -0,0 +1,89 @@
+﻿using AGSO.Network;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WinSock.WSAData wsa;
+            WinSock.WSAStartup(0x22, out wsa);
+
+            Console.WriteLine("c/s:");
+            if (Console.ReadKey().Key == ConsoleKey.C)
+            {
+                ClientMain();
+            }
+            else
+            {
+                ServerMain();
+            }
+        }
+
+        private static void ClientMain()
+        {
+            IntPtr s = WinSock.socket(WinSock.AF_INET, WinSock.SOCK_DGRAM, WinSock.IPPROTO_UDP);
+
+            int val = 1;
+            WinSock.ioctlsocket(s, 0x8004667e, ref val);
+
+            WinSock.sockaddr_in addr = new WinSock.sockaddr_in();
+            addr.sin_family = WinSock.AF_INET;
+            addr.sin_port = WinSock.htons(10800);
+            addr.sin_addr = WinSock.inet_addr("127.0.0.1");
+
+            int buffer_len = 64;
+            IntPtr buffer = Marshal.AllocHGlobal(buffer_len);
+            for (int i = 0; i < buffer_len; ++i)
+            {
+                Marshal.WriteByte(buffer, i, (byte)i);
+            }
+
+            WinSock.sendto(s, buffer, buffer_len, 0, ref addr, WinSock.sockaddr_in.Size);
+
+            WinSock.closesocket(s);
+        }
+
+        private static void ServerMain()
+        {
+            IntPtr s = WinSock.socket(WinSock.AF_INET, WinSock.SOCK_DGRAM, WinSock.IPPROTO_UDP);
+
+            int val = 1;
+            WinSock.ioctlsocket(s, 0x8004667e, ref val);
+
+            WinSock.sockaddr_in addr = new WinSock.sockaddr_in();
+            addr.sin_family = WinSock.AF_INET;
+            addr.sin_port = WinSock.htons(10800);
+            addr.sin_addr = 0; //any
+
+            WinSock.bind(s, ref addr, WinSock.sockaddr_in.Size);
+
+            int buffer_len = 64;
+            IntPtr buffer = Marshal.AllocHGlobal(buffer_len);
+            while (true)
+            {
+                WinSock.sockaddr_in recvaddr = new WinSock.sockaddr_in();
+                int addrlen = WinSock.sockaddr_in.Size;
+                if (WinSock.recvfrom(s, buffer, buffer_len, 0, ref recvaddr, ref addrlen) >= 0)
+                {
+                    break;
+                }
+                var e = Marshal.GetLastWin32Error();
+                if (e != 10035)
+                {
+                    Console.WriteLine(e);
+                    break;
+                }
+                System.Threading.Thread.Sleep(1);
+            }
+
+            WinSock.closesocket(s);
+        }
+    }
+}
